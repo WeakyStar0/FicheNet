802bd3542acca9eed409b447082253149219c2a6
x1cuu <francisco.fernandes0811@gmail.com>
2025-04-29 17:53:27 +0100

    ola

--- projectroot/client/src/Pages/Home.js
+++ projectroot/client/src/Pages/Home.js
@@ -91,7 +91,7 @@ export const Home = () => {
                             <div className="slide-content">
                                 <h1>Bem Vindo</h1>
                                 <p>
-                                    Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                                    Lorem Ipsumis simply dummy text of the printing and typesetting industry. 
                                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                                     when an unknown printer took a galley of type and scrambled it to make a type specimen book.
                                 </p>
